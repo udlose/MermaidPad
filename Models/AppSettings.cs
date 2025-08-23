4915d0da82c5c37317f4236aeee868150b7ea7a3
Dave Black <656118+udlose@users.noreply.github.com>
2025-08-23 16:12:57 -0500

    Update MermaidJS version from 11.9.0 to 11.10.1, set initial value of AutoUpdateMermaid to false until I change this to a background process

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -3,9 +3,9 @@ namespace MermaidPad.Models;
 public class AppSettings
 {
     public string? LastDiagramText { get; set; }
-    public string BundledMermaidVersion { get; set; } = "11.9.0";
+    public string BundledMermaidVersion { get; set; } = "11.10.1";
     public string? LatestCheckedMermaidVersion { get; set; }
-    public bool AutoUpdateMermaid { get; set; } = true;
+    public bool AutoUpdateMermaid { get; set; }
     public bool LivePreviewEnabled { get; set; } = true;
     public int EditorSelectionStart { get; set; }
     public int EditorSelectionLength { get; set; }
